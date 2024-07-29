2031aeda549c6997bdbcc152905ca196bbde60ae
acurzon <adam@curzon.org>
2024-07-29 17:13:09 +0100

    feat: implement redis cache to post

--- src/main/java/com/adamcurzon/api/post/PostController.java
+++ src/main/java/com/adamcurzon/api/post/PostController.java
@@ -3,31 +3,34 @@ package com.adamcurzon.api.post;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
+import java.math.BigInteger;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 public class PostController {
     private final PostRepository postRepository;
 
-    public PostController(PostRepository postRepository){
+    public PostController(PostRepository postRepository)
+    {
         this.postRepository = postRepository;
     }
 
     @GetMapping("/")
-    public List<Post> getTest(){
+    public List<Post> index()
+    {
         return this.postRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Post view(@PathVariable BigInteger id)
+    {
+        return this.postRepository.findById(id);
     }
 
     @PostMapping("/")
     @ResponseStatus(HttpStatus.CREATED)
-    public void create(@RequestBody Post post){
+    public void create(@RequestBody Post post)
+    {
         this.postRepository.create(post);
-    }
-
-    @GetMapping("/womp")
-    public Map<String, String> womp(){
-        return Map.of("womp", "womp");
     }
 }
